dd6cf481aa95e3acfd6c4fff174aa90765ef3473
yifanwu <yifan1030@gmail.com>
2019-07-12 19:02:32 -0700

    some comments and changed test

--- tests/endToEndTest.ts
+++ tests/endToEndTest.ts
@@ -12,8 +12,12 @@ function clientLog(m: string) {
 
 /**
  * we are going to first create the server instance, and mock some connections
+ * follow the INSTRUCTIONS comments to run locally
+ *
+ * @richard: Currently, the `exec` command hangs
  */
 export async function testPostgresEndToEnd() {
+  // INSTRUCTIONS: change the dbName here to an instance you have locally
   const dbName = "postgresTest";
   const message = {
     dbName
@@ -30,15 +34,27 @@ export async function testPostgresEndToEnd() {
       action: "open",
       message
     }));
+    client.send(JSON.stringify({
+      id: "testSetUp",
+      action: "exec",
+      sql: `drop table if exists testTable;
+            create table testTable (user_id integer);
+            insert into testTable values (1), (2), (3);`,
+      message
+    }));
     // I think this is synchronous?
     client.send(JSON.stringify({
       id: "testExec",
       action: "exec",
-      sql: "select * from small_log limit 5",
+      sql: "select * from testTable limit 5",
       message
     }));
   }, 100);
   client.on("message", (message: string) => {
     clientLog(`message\n---------\n${JSON.stringify(message)}\n--------\n`);
+    const parsedMessage = JSON.parse(message);
+    if (parsedMessage.id === "testExec") {
+      // @richard the values should be 1,2,3
+    }
   });
 }
