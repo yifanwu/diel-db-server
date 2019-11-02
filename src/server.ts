99a1e8f5012898bbab6b3e5e113773898e8c920a
Lucie Choi <lucie0407@berkeley.edu>
2019-11-02 14:40:08 -0700

    finished cleanup testing for both dbdrivers

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import * as http from "http";
 import * as WebSocket from "ws";
 import { LogWarning, LogInfo, LogError, TraceEvents } from "./messages";
-import { DbNameType, DbCon, SocketIdType, DbConfig } from "./types";
+import { DbNameType, DbCon, SocketIdType, DbConfig, DbDriver } from "./types";
 import { CloseDb, OpenDb, RunToDb, QueryFromDb } from "./helper";
 
 interface DielMessage {
@@ -85,10 +85,23 @@ export function StartDielDbServer(configs: DbConfig[]) {
           const dbCon = dbs.get(dbName);
           if (dbCon) {
             if (dbCon.cleanUpQueries) {
-              const queries = dbCon.cleanUpQueries.split(";");
-              for (let q of queries) {
-                const results = await QueryFromDb(dbCon, q + ";");
-                TraceEvents(`Result for exec action\n-------------\n${JSON.stringify(results, null, 2)}\n----------------`);
+              switch(dbCon.driver) {
+                case DbDriver.Postgres: {
+                  const queries = dbCon.cleanUpQueries.split(";");
+                  for (let q of queries) {
+                    const results = await QueryFromDb(dbCon, q + ";");
+                    TraceEvents(`Result for exec action\n-------------\n${JSON.stringify(results, null, 2)}\n----------------`);
+                  }
+                  break;
+                }
+                case DbDriver.SQLite: {
+                  TraceEvents(`Clean up \n${dbCon.cleanUpQueries}`);
+                  RunToDb(dbCon, dbCon.cleanUpQueries);
+                  break;
+                }
+                default: {
+                  RunToDb(dbCon, dbCon.cleanUpQueries);
+                }
               }
             }
             CloseDb(dbCon);
